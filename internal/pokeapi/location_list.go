d8036e5df8960fed281311bb564f2c11675001a1
tnaums <tanaumann@outlook.com>
2026-01-12 00:38:04 -0600

    added reapLoop and reap

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -13,25 +13,28 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		url = *pageURL
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
+	dat, ok := c.cache.Get(url)
+	if !ok {
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
 
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
-	defer resp.Body.Close()
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+		defer resp.Body.Close()
 
-	dat, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
+		dat, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
 
-	c.cache.Add(url, dat)
+		c.cache.Add(url, dat)
+	}
 	locationsResp := RespShallowLocations{}
-	err = json.Unmarshal(dat, &locationsResp)
+	err := json.Unmarshal(dat, &locationsResp)
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -17,10 +17,12 @@ type Cache struct {
 }
 
 func NewCache(interval time.Duration) Cache {
-	return Cache{
+	x := Cache{
 		pokeCache: make(map[string]cacheEntry),
 		mux: sync.Mutex{},
 	}
+	go x.reapLoop(interval)
+	return x
 }
 
 
@@ -36,9 +38,29 @@ func (c *Cache) Add(key string, value []byte) {
 }
 
 func (c *Cache) Get(url string) ([]byte, bool) {
+	c.mux.Lock()
 	dat, ok := c.pokeCache[url]
+	c.mux.Unlock()
 	if ok {
+		fmt.Println("Found in cache!")
 		return dat.val, true}
+	fmt.Println("Nothing in cache!")
 	return dat.val, false
 }
 
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	for k, v := range c.pokeCache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.pokeCache, k)
+		}
+	}
+}

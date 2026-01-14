21169818c4d57c4ccd6e049d4207d5f202af5eff
tnaums <tanaumann@outlook.com>
2026-01-14 00:35:34 -0600

    progress with catch

--- command_catch.go
+++ command_catch.go
@@ -1,13 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func commandCatch(cfg *config, pokemon string) error {
-	_, err := cfg.pokeapiClient.CatchPokemon(pokemon)
+	pokemonData, err := cfg.pokeapiClient.CatchPokemon(pokemon)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("Trying to catch %s\n", pokemon)
-	//	fmt.Println(pokemonData)
+	fmt.Printf("Base experience %d\n", pokemonData.BaseExperience)
+	// 1 to 255
+	chance := rand.Intn(254) + 1
+	fmt.Printf("Base: %d\n", pokemonData.BaseExperience)
+	fmt.Printf("Random: %d\n", chance)
+	if pokemonData.BaseExperience > chance {
+		fmt.Println("Missed!")
+	} else {
+		fmt.Println("Caught!")
+	}
 	return nil
 }
